2e0d78fd17fc3e321657bf252618eb774a21c1de
Diegolotr99 <diegolotr99@gmail.com>
2020-04-23 19:46:36 -0600

    2 files modified

--- index.js
+++ index.js
@@ -70,10 +70,12 @@ firebase.auth().onAuthStateChanged((user)=> {
     displayNameText.textContent = user.displayName
     userContainer.style.display = "flex";
     guestbookContainer.style.display = "block";
+    subscribeGuestbook()
   } else {
     startRsvpButton.textContent = "RSVP"
     userContainer.style.display = "none";
     guestbookContainer.style.display = "none";
+    unsubscribeGuestbook()
   }
 });
 
@@ -90,3 +92,21 @@ form.addEventListener("submit", (e)=> {
   input.value = "";
   return false;
 });
+function subscribeGuestbook(){
+  guestbookListener = firebase.firestore().collection("guestbook").orderBy("timestamp", "desc")
+  .onSnapshot((snaps) => {
+    guestbook.innerHTML = "";
+    snaps.forEach((doc) =>{
+        const entry = document.createElement("p");
+        entry.textContent = doc.data().name + ": " + doc.data().text;
+        guestbook.appendChild(entry);
+    })
+  });
+
+  function unsubscribeGuestbook(){
+    if (guestbookListener != null){
+      guestbookListener();
+      guestbookListener = null;
+    }
+  }
+}
